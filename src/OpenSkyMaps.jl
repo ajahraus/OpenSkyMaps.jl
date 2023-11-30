7ec6949f70525c304ba48917451c591323a3c155
Adam Jahraus <adam.jahraus@hexagon.com>
2023-11-30 11:59:57 -0700

    Change just about everything

--- src/OpenSkyMaps.jl
+++ src/OpenSkyMaps.jl
@@ -1,8 +1,10 @@
 module OpenSkyMaps
 using Meshes
 using LinearAlgebra: dot
+using Base.Iterators: take
+using Base.Iterators: map as imap
 
-export OpenSkyMap, polarFromCartesian, project_to_polar
+export OpenSkyMap, polarFromCartesian, project_to_polar, filter_height, filter_normal, sample_normal, sample_coordinate, clamp_z
 
 
 struct OpenSkyMap
@@ -13,69 +15,40 @@ struct OpenSkyMap
     OpenSkyMap() = new(BitMatrix(hypot(indx[1], indx[2]) < 512 for indx in CartesianIndices((-512:511, -512:511))))
 end
 
-function polarFromCartesian(point)
+function polarFromCartesian(point::Point3f)::Point2f
     all(iszero, point.coords[1:2]) && return Point(0, 0)
     ρ = hypot(point.coords...)
     φ = acos(point.coords[3] / ρ)
-    return Point(point.coords[1:2] .* (2φ / (π * ρ))...)
+    return Point2f(point.coords[1:2] .* (2φ / (π * ρ))...)
 end
 
-function filter_normals(geometry)
-    normals = geometry |>
-              elements .|>
-              simplexify .|>
-              first .|>
-              normal
+sample_normal = normal ∘ first ∘ simplexify
+sample_coordinate = coordinates ∘ first
 
-    sample_coordinates = geometry .|>
-                         vertices .|>
-                         first .|>
-                         coordinates
-    facing_bit_vector = dot.(normals, sample_coordinates) .< 0.0
-    return facing_bit_vector
+filter_normal(el, vert) = sample_normal(el) ⋅ sample_coordinate(vert) < 0.0
+
+filter_height(v::NTuple{3,Point3f}) = any(c[3] > 0.0f0 for c in coordinates.(v))
+
+function collect_valid_faces(geometry::Mesh)
+    els = elements(geometry)
+    verts = vertices.(geometry)
+    filter_bv = @. filter_height(verts) && filter_normal(els, verts)
+    [f for (f, b) in zip(els, filter_bv) if b]
 end
 
-function filter_height(geometry)
-    below_horizon = geometry .|>
-                    vertices .|>
-                    (v -> coordinates.(v)) .|>
-                    (v -> getindex.(v, 3)) .|>
-                    splat(max) .> 0.0
-    return below_horizon
-end
-
-function collect_valid_faces(geometry, observer)
-    geometry_t = geometry |>
-                 Translate(-observer.coords...)
-    filter_bv = filter_height(geometry_t) .&& filter_normals(geometry_t)
-    [f for (f, b) in zip(elements(geometry_t), filter_bv) if b]
-end
-
-function clamp_z(point)
+function clamp_z(point::Point3f)::Point3f
     c = coordinates(point)
-    Point(c[1], c[2], max(c[3], 0.0))
+    c[3] ≥ 0.0 ? point : Point3f(c[1], c[2], 0.0)
 end
 
-function clamp_vertices_to_horizon(remaining_faces)
-    faces_needing_clamping = remaining_faces .|>
-                             vertices .|>
-                             (v -> coordinates.(v)) .|>
-                             (v -> getindex.(v, 3)) .|>
-                             splat(min) .< 0.0
-    clamped_faces = [b ? Ngon(clamp_z.(vertices(f))...) : f for (f, b) in zip(remaining_faces, faces_needing_clamping)]
-    return clamped_faces
-end
+face_needs_clamp(face) = any(coordinates(v)[3] < 0.0 for v in vertices.(face))
 
-function satellite_unit_vector(φ, θ)
-    z, r = sincos(φ)
-    y, x = r .* sincos(θ)
-    Point3f(x, y, z)
-end
+clamp_vertices_to_horizon(f) = face_needs_clamp(f) ? Ngon(clamp_z.(vertices(f))...) : f
 
-function project_to_polar(observer, geometry)
-    remaining_faces = collect_valid_faces(geometry, observer)
-    clamped_faces = clamp_vertices_to_horizon(remaining_faces)
-
+function project_to_polar(observer::Point3f, geometry::Mesh)
+    geometry = geometry |> Translate(-observer.coords...)
+    remaining_faces = collect_valid_faces(geometry)
+    clamped_faces = clamp_vertices_to_horizon.(remaining_faces)
     sphere_faces = [Ngon(polarFromCartesian.(vertices(f))...) for f in clamped_faces]
     return sphere_faces
 end
--- src/demo.jl
+++ src/demo.jl
@@ -4,12 +4,12 @@ using Meshes
 import GLMakie
 
 function main()
-    observer = Point3f(15f0, -5f0, 0f0)
+    observer = Point3f(15.0f0, -5.0f0, 0.0f0)
 
-    geometry1 = Box((-0.5f0, 0f0, 0f0), (0.5f0, 1f0, 10f0)) |>
+    geometry1 = Box((-0.5f0, 0.0f0, 0.0f0), (0.5f0, 1.0f0, 10.0f0)) |>
                 boundary |>
                 (g -> refine(g, TriRefinement())) |>
-                (g -> refine(g,  TriRefinement())) 
+                (g -> refine(g, TriRefinement()))
 
     geometries = [geometry1 |> Translate((rand(Float32) + 3.0f0 * x, rand(Float32) + -10.0f0 * y, 0.0f0)) for x in 0:10 for y in 0:1]
 
@@ -22,9 +22,9 @@ function main()
         viz!(fig[1, 1], geometry, showfacets=true)
 
 
-        viz(fig[1, 2], Sphere((0f0, 0f0), 1.0f0), color=:red, aspect=1)
-        viz!(fig[1, 2], Sphere((0f0, 0f0), 0.01f0))
-        viz!(fig[1, 2], reduce(merge,polar_faces), showfacets=true)
+        viz(fig[1, 2], Sphere((0.0f0, 0.0f0), 1.0f0), color=:red, aspect=1)
+        viz!(fig[1, 2], Sphere((0.0f0, 0.0f0), 0.01f0))
+        viz!(fig[1, 2], reduce(merge, polar_faces), showfacets=true)
         display(fig)
     end
     return polar_faces
